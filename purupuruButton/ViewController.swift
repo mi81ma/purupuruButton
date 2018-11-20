17ea9a90991340fe4dc28fbadea2796b53be3c16
masato <masato@m-mbp.local>
2018-11-20 18:02:02 +0800

    Initial Commit

---
+++ purupuruButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  purupuruButton
+//
+//  Created by masato on 20/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
